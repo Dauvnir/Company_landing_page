8544d09bac4bbed111bacd65216dae85dd84b8e9
Dauvnir <pdawidpolak@gmail.com>
2025-07-13 20:14:34 +0200

    Added services section

---
+++ src/MainComponents/ServicesSection.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import Services from "../Components/Services";
+
+const Wrapper = styled.section`
+	display: flex;
+	flex-direction: column;
+
+	width: 100%;
+	height: auto;
+
+	padding-inline: clamp(0.5rem, -0.122rem + 3.31vw, 2rem); //300 px to 1024px calculated
+	margin-block: 2rem; // to change
+
+	h3 {
+		width: 100%;
+		text-align: center;
+	}
+`;
+
+const ServicesSection = () => {
+	return (
+		<Wrapper>
+			<h3>Services</h3>
+			<Services />
+		</Wrapper>
+	);
+};
+
+export default ServicesSection;
