196b098dd42fac8193a41da139580ee4afb5ced4
Dauvnir <pdawidpolak@gmail.com>
2025-07-13 19:13:21 +0200

    Added welcome section

---
+++ src/MainComponents/WelcomeSection.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import NavigationMenu from "../Components/NavigationMenu";
+import WelcomeMessage from "../Components/WelcomeMessage";
+const Wrapper = styled.section`
+	background-image: url("background.jpg");
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: cover;
+
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+`;
+
+const WelcomeSection = () => {
+	return (
+		<Wrapper>
+			<NavigationMenu />
+			<WelcomeMessage />
+		</Wrapper>
+	);
+};
+
+export default WelcomeSection;
